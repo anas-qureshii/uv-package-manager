bbf006fd2067110c697125167115a0113e102ce8
anasqureshi <incredibleinfo333@gmail.com>
2025-05-24 20:06:25 +0500

    adding python project built using uv package manager

---
+++ fourthclass.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.title("Unit Converter")
+
+st.badge("new start")
+
+# Add button
+
+if st.button("click me"):
+    st.write("Button clicked!")
+else:
+    st.write("Button not clicked!")
